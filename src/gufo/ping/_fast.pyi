f4a7a7ae893e706b004dd69e64406906679232e3
Dmitry Volodin <dv@nocproject.org>
2022-11-09 09:30:22 +0100

    Rename _fast.py to _fast.pyi

--- src/gufo/ping/_fast.pyi
+++ src/gufo/ping/_fast.pyi
@@ -1,12 +1,10 @@
 # ---------------------------------------------------------------------
 # Gufo Ping: _fast stub
-# This is a stub file for _pyo3 module only to statisfy
+# This is a stub file for pyo3 module only to satisfy
 # the mkdocstings
 # ---------------------------------------------------------------------
 # Copyright (C) 2022, Gufo Labs
 # ---------------------------------------------------------------------
 
-
 class SocketWrapper(object):
-    def __init__(self, afi: int) -> None:
-        ...
+    def __init__(self, afi: int) -> None: ...
