0e3b82e07d1cc4a3e768fa08fd69fe6ba528975f
Dmitry Volodin <dv@nocproject.org>
2022-11-14 09:16:32 +0100

    Verbose build

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from setuptools_rust import RustExtension
 
 setup(
-    rust_extensions=[RustExtension("gufo.ping._fast"), args=["verbose"]],
+    rust_extensions=[RustExtension("gufo.ping._fast", args=["--verbose"])],
 )
