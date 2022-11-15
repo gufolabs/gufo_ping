586469daf6b448197f54a6f008d4926d78beed08
Dmitry Volodin <dv@nocproject.org>
2022-11-15 18:04:50 +0100

    Set docker platform during build

--- tools/build/setup-rust.sh
+++ tools/build/setup-rust.sh
@@ -13,7 +13,7 @@ if [ -z "${RUST_ARCH}" ]; then
     exit 2
 fi
 
-RUST_VERSION=1.65.0
+RUST_VERSION=${RUST_VERSION:-1.65.0}
 
 # @todo: Allow override
 export RUSTUP_HOME=${RUSTUP_HOME:-/usr/local/rustup}
@@ -26,19 +26,15 @@ echo "RUSTUP_HOME = ${RUSTUP_HOME}"
 echo "CARGO_HOME  = ${CARGO_HOME}"
 
 # Install rust
-curl -s --tlsv1.2 -o rustup-init https://sh.rustup.rs
 # rustup-init tries to check /proc/self/exe
 # which is not accessible during Docker build
-# on aarch64
-# echo "Patching rustup"
-# sed -i.bak 's#/proc/self/exe#/bin/sh#g' rustup-init
-# rm rustup-init.bak
-#
-chmod a+x rustup-init
-./rustup-init -y --no-modify-path --profile minimal\
-    --default-toolchain ${RUST_VERSION}\
-    --default-host ${RUST_ARCH}
-rm rustup-init
+# on aarch64, so we will patch it
+curl -s --tlsv1.2 https://sh.rustup.rs \
+    | sed 's#/proc/self/exe#/bin/sh#g' \
+    | sh -s -- \
+        -y --no-modify-path --profile minimal \
+        --default-toolchain ${RUST_VERSION} \
+        --default-host ${RUST_ARCH}
 # Check
 cargo --version
 rustc --version
